12a218c4aa5e7fa98c8aa6bc7b83fcbb06727c00
Diederik de Groot <dkgroot@talon.nl>
2019-10-19 18:48:00 +0200

    Cleanup gen_call.php Add looping to generate multiple calls in sequence Add gen_call.cfg to store the static configuration for the call to generate
    Signed-off-by: Diederik de Groot <dkgroot@talon.nl>

--- gen_call.php
+++ gen_call.php
@@ -1,17 +1,17 @@
 #!/usr/bin/env php
 <?php
 /**
- * @category   Asterisk / DeviceState / Pami
- * @package    Simple DevState Monitor
+ * @category   Asterisk / Generate Call / Pami
+ * @package    Simple Call Generator
  * @author   Diederik de Groot <ddegroot@talon.nl>
  * @license  http://github.com/dkgroot/PAMI Apache License 2.0
  *
  * Copyright 2016 Diederik de Groot <ddegroot@talon.nl>
  */
-if ($argc <5 ) {
-    echo "Use: $argv[0] <host> <port> <user> <pass>] ";
-    echo "example: ./call_gen.php 192.168.1.20 5038 admin secret
-" ;
+if ($argc > 1) {
+    echo "Usage: $argv[0]] ";
+    echo "Purpose: Generate a number of subsequent calls from calling_device to recipient_line, answer the incoming call, hangup.";
+    echo "edit gen_call.cfg to set parameters";
     exit (254);
 }
 
@@ -45,9 +45,11 @@ class EventListener implements IEventListener
 {
     private $call_info;
     private $pami;
+    private $loop;
     
     public function __construct($call_info) {
         $this->call_info = $call_info;
+        $this->loop = $call_info['number_of_loops'];
     }
     
     public function setPami($pami) {
@@ -56,56 +58,54 @@ class EventListener implements IEventListener
     
     public function handle(EventMessage $event)
     {
-        //This Handler will print the incoming message (only for the event we are interested in).
-        //var_dump($event);
-        /*
-        if ($event instanceof PAMI\Message\Event\DeviceStateChangeEvent) {
-            echo("Got a new DeviceStateChangeEvent\n");
-            //var_dump($event);
-            echo(" - Device:".$event->getDevice()."\n");
-            echo(" - State:".$event->getState()."\n");
-        }
-        */
-        /*
-        if ($event instanceof PAMI\Message\Event\NewchannelEvent && $event->getLinkedId() == $this->call_info['linkedid']) {
-            echo("Got a new NewchannelEvent\n");
-            echo(" - Channel:".$event->getChannel()."\n");
-            echo(" - Linkedid:".$event->getLinkedId()."\n");
-        }
-        */
-        if ($event instanceof PAMI\Message\Event\NewstateEvent && $event->getLinkedId() == $this->call_info['linkedid']) {	// found the event we were waiting for
-            $channel = $event->getChannel();
-            $exten = $event->getExten();
-            $channelState = $event->getChannelStateDesc();
-            // Answer the call
-            try {
-                if ($exten == $this->call_info['called_line']) {
+        $call_info = $this->call_info;
+        $pami = $this->pami;
+        try {
+            if ($event instanceof PAMI\Message\Event\NewstateEvent && $event->getLinkedId() == $this->call_info['linkedid']) {	// Wait for a ChannelState for our LinkedId
+                $channel = $event->getChannel();
+                $exten = $event->getExten();
+                $channelState = $event->getChannelStateDesc();
+                if ($exten == $call_info['recipient_line']) {
                     echo("Got a new NewstateEvent\n");
                     echo(" - Channel:".$channel."\n");
                     echo(" - ChannelStateDesc:".$channelState."\n");
                     echo(" - Linkedid:".$event->getLinkedId()."\n");
                     echo(" - Exten:".$exten."\n");
                     if ($channelState === "Ringing") {										// Answer the call
-                        $action = new SCCPAnswerCallAction($event->getChannel(), $this->call_info['called_device']);
+                        $action = new SCCPAnswerCallAction($event->getChannel(), $call_info['recipient_device']);
                         $action->setActionId("1432.124");
-                        $response = $this->pami->send($action);
+                        $response = $pami->send($action);
                     }
-
                     if ($channelState === "Up") {										// Hangup the call
                         $action = new HangupAction($channel);
                         $action->setActionId("1432.125");
-                        $response = $this->pami->send($action);
+                        $response = $pami->send($action);
                         
-                        // logoff pami immediately so we stop fast
-                        $action = new LogoffAction();
-                        $response = $this->pami->send($action);
                     }
                 }
-            } catch (Exception $e) {
-                //echo $e->getMessage() . "\n";
             }
+            if ($event instanceof PAMI\Message\Event\DeviceStateChangeEvent && $event->getDevice() == "SCCP/".$call_info['calling_line']) {
+                echo("Got a new DeviceStateChangeEvent\n");
+                echo(" - Device:".$event->getDevice()."\n");
+                echo(" - State:".$event->getState()."\n");
+                if ($event->getState() == "NOT_INUSE") {
+                    usleep(1000000);												// Give phones a little time to come to standstill
+                    echo "Next Call:" . $this->loop . "\n";
+                    if ($this->loop--) {											// Generate Next Call
+                        $action = new SCCPStartCallAction($call_info['calling_device'], $call_info['calling_line'], $call_info['recipient_line']);
+                        $action->setActionId("1432.123");
+                        $action->setLinkedId($call_info['linkedid']);
+                        $response = $pami->send($action);
+                    } else {													// logoff pami immediately so we stop fast
+                        $action = new LogoffAction();
+                        $response = $pami->send($action);
+                    }
+                }
+            }
+            //var_dump($event);
+        } catch (Exception $e) {
+            //echo $e->getMessage() . "\n";
         }
-        //var_dump($event);
     }
 }
 
@@ -118,45 +118,29 @@ ini_set('display_errors', 1);
 
 try
 {
-    $options = array(
-        'host' => $argv[1],
-        'port' => $argv[2],
-        'username' => $argv[3],
-        'secret' => $argv[4],
-
-        'connect_timeout' => 10,
-        'read_timeout' => 3
-    );
+    require('gen_call.cfg');
     echo("Connecting to host:".$options['host'].":".$options['port']."\n");
     
     $pami = new ClientImpl($options);
-
-    // read from gen_call.ini instead of static definition
-    $call_info = array(
-        'caller_device' => "SEPE0D173E11D95",
-        'called_device' => "SEP0023043403F9",
-        'caller_line' => "98031",
-        'called_line' => "98041",
-        'linkedid' => "900001",
-    );
-    
     $eventListener = new EventListener($call_info);
     $pami->registerEventListener($eventListener);
     $pami->open();
     $eventListener->setPami($pami);
+    echo("Connected\n");
 
-    $action = new SCCPStartCallAction($call_info['caller_device'], $call_info['caller_line'], $call_info['called_line']);
+    // Generate the initial call
+    $action = new SCCPStartCallAction($call_info['calling_device'], $call_info['calling_line'], $call_info['recipient_line']);
     $action->setActionId("1432.123");
     $action->setLinkedId($call_info['linkedid']);
     $response = $pami->send($action);
 
     // wait for AMI event and handle the rest in the EventListener
     $time = time();
-    while($pami && (time() - $time) < $options['connect_timeout'])   // start waiting for events (for ;connect_timeout' minute)
-    {
+    do {
         usleep(10000);					// wait 10 ms
         $pami->process();				// poll pami to see if anything happened
-    }
+    } while($pami);
+
     if (!is_null($pami)) {
         $pami->close(); // send logoff and close the connection.
     }
